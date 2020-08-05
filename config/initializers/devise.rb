87c4546fdbbe0c104547838116137bae75eb57e6
zetavg <mail@zeta.vg>
2020-08-06 00:35:16 +0800

    construct sign in view

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -312,3 +312,7 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 end
+
+Rails.application.config.to_prepare do
+  Devise::SessionsController.layout 'basic'
+end
