ca53245c4933719234601c49776444a855de91d4
zetavg <mail@zeta.vg>
2020-08-05 23:26:59 +0800

    construct flash messages area

---
+++ app/helpers/flash_messages_helper.rb
@@ -0,0 +1,11 @@
+module FlashMessagesHelper
+  def class_for_flash_message(flash_type)
+    {
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-danger',
+      warning: 'alert-warning',
+      notice: 'alert-info'
+    }[flash_type.to_sym] || 'alert-info'
+  end
+end
