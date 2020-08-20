f24539b4300c86d368544a803ca4da2b8240c3bb
zetavg <mail@zeta.vg>
2020-08-20 23:26:51 +0800

    setup user_cash, user_account accounts and deposit, withdraw transfers

--- config/initializers/double_entry.rb
+++ config/initializers/double_entry.rb
@@ -1,22 +1,30 @@
 # frozen_string_literal: true
 
+Money.default_currency = Money::Currency.new(BentOS::Config.accounting.system_currency)
+
 require 'double_entry'
 
 DoubleEntry.configure do |config|
   # Use json(b) column in double_entry_lines table to store metadata instead of separate metadata table
   config.json_metadata = true
 
-  # config.define_accounts do |accounts|
-  #   user_scope = ->(user) do
-  #     raise 'not a User' unless user.class.name == 'User'
-  #     user.id
-  #   end
-  #   accounts.define(:identifier => :savings,  :scope_identifier => user_scope, :positive_only => true)
-  #   accounts.define(:identifier => :checking, :scope_identifier => user_scope)
-  # end
-  #
-  # config.define_transfers do |transfers|
-  #   transfers.define(:from => :checking, :to => :savings,  :code => :deposit)
-  #   transfers.define(:from => :savings,  :to => :checking, :code => :withdraw)
-  # end
+  config.define_accounts do |accounts|
+    user_scope = lambda do |user|
+      raise 'not a User' unless user.class.name == 'User'
+
+      user.id
+    end
+
+    # Representing the user's own money.
+    # The balance should always be not more than zero, since users will only spend money in this system.
+    accounts.define(scope_identifier: user_scope, identifier: :user_cash, negative_only: true)
+    # The money in this account can be used to pay user's orders.
+    # The balance may be a negative value if the user choise to pay after the order has been compeleted.
+    accounts.define(scope_identifier: user_scope, identifier: :user_account)
+  end
+
+  config.define_transfers do |transfers|
+    transfers.define(from: :user_cash, to: :user_account, code: :deposit)
+    transfers.define(from: :user_account, to: :user_cash, code: :withdraw)
+  end
 end
